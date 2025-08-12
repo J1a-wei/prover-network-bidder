52c683453703b203021c3ace80e3d263d7d86781
Elliott He <elliott.he@outlook.com>
2025-08-12 17:14:11 +0800

    prover network client

--- client/prover_network_client.go
+++ client/prover_network_client.go
@@ -41,3 +41,49 @@ func (c *ProverNetworkClient) RegisterApp(appId, info string, elf []byte) error 
 	}
 	return nil
 }
+
+func (c *ProverNetworkClient) EstimateCost(appId string, inputs [][]byte) (cost uint32, pvDigest []byte, err error) {
+	resp, err := serviceapi.NewProverNetworkClient(c.conn).EstimateCost(context.Background(), &serviceapi.EstimateCostRequest{
+		AppId:  appId,
+		Inputs: inputs,
+	})
+	if err != nil {
+		return 0, []byte{}, err
+	}
+	if resp.GetErr() != nil {
+		log.Errorf("EstimateCost err: %s", resp.GetErr().GetMsg())
+		return 0, []byte{}, fmt.Errorf("EstimateCost err: %s", resp.GetErr().GetMsg())
+	}
+	return resp.Cost, resp.PvDigest, nil
+}
+
+func (c *ProverNetworkClient) ProveTask(appId, taskId string, inputs [][]byte) error {
+	resp, err := serviceapi.NewProverNetworkClient(c.conn).ProveTask(context.Background(), &serviceapi.ProveTaskRequest{
+		AppId:  appId,
+		TaskId: taskId,
+		Inputs: inputs,
+	})
+	if err != nil {
+		return err
+	}
+	if resp.GetErr() != nil {
+		log.Errorf("ProveTask err: %s", resp.GetErr().GetMsg())
+		return fmt.Errorf("ProveTask err: %s", resp.GetErr().GetMsg())
+	}
+	return nil
+}
+
+func (c *ProverNetworkClient) GetProvingResult(appId, taskId string, inputs [][]byte) (proof []byte, err error) {
+	resp, err := serviceapi.NewProverNetworkClient(c.conn).GetProvingResult(context.Background(), &serviceapi.GetProvingResultRequest{
+		AppId:  appId,
+		TaskId: taskId,
+	})
+	if err != nil {
+		return []byte{}, err
+	}
+	if resp.GetErr() != nil {
+		log.Errorf("GetProvingResult err: %s", resp.GetErr().GetMsg())
+		return []byte{}, fmt.Errorf("GetProvingResult err: %s", resp.GetErr().GetMsg())
+	}
+	return resp.Proof, nil
+}
